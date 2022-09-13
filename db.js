94ddc1796524dd57d5a1d9a075a5adabdef41572
Nick Maslov <kategorychnost@gmail.com>
2022-09-13 13:25:13 -0400

    created node express server, connected mongodb

---
+++ db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+function connectDB() {
+    mongoose.connect(
+        'mongodb+srv://admin:admin12345@cluster0.icqu1dj.mongodb.net/guest-house',
+        {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        }
+    );
+    const connection = mongoose.connection;
+
+    connection.on('connected', () => {
+        console.log('MongoDB connection successful!');
+    });
+
+    connection.on('error', () => {
+        console.log('MongoDB connection Failed!');
+    });
+}
+
+connectDB();
+
+module.exports = mongoose;
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+const dbConnection = require('./db');
+
+app.get('/', (req, res) => res.send('Hello world!'));
+app.listen(port, () => console.log(`Node JS Server Started on port ${port}`));
